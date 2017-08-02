676cafe17e22b3b780d9d55027f61a167342a410
Zachary Adam Kaplan <razic@viralkitty.com>
2017-08-02 20:53:49 +0000

    add status

---
+++ status.go
@@ -0,0 +1,8 @@
+package main
+
+// Status data struct
+type Status struct {
+	Application  string `json:"Application"`
+	Version      string `json:"Version"`
+	SuccessCount int    `json:"Success_Count"`
+}

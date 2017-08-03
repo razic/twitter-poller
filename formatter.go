187f992340dc46647226ceaad809d307ec635377
Zachary Adam Kaplan <razic@viralkitty.com>
2017-08-03 21:14:52 +0000

    update formatter

--- formatter.go
+++ formatter.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"io"
 	"strconv"
 	"text/tabwriter"
@@ -17,13 +16,11 @@ func NewFormatter() *Formatter {
 
 // Format takes a map of strings to ints, and formats them for human reading
 func (f *Formatter) Format(writer io.Writer, data map[string]int) {
-	var buf bytes.Buffer
+	w := tabwriter.NewWriter(writer, 0, 0, 4, ' ', tabwriter.TabIndent)
 
 	for k, v := range data {
-		buf.WriteString(k + "\t" + strconv.Itoa(v) + "\n")
+		w.Write([]byte(k + "\t" + strconv.Itoa(v) + "\n"))
 	}
 
-	w := tabwriter.NewWriter(writer, 0, 0, 4, ' ', tabwriter.TabIndent)
-	w.Write(buf.Bytes())
 	w.Flush()
 }

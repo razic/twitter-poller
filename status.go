70b3ec255c2be41fa1e7a63db8226aaaf7690a13
Zachary Adam Kaplan <razic@viralkitty.com>
2017-08-03 02:08:03 +0000

    :lipstick:

--- status.go
+++ status.go
@@ -2,7 +2,7 @@ package main
 
 // Status data struct
 type Status struct {
-	Application  string `json:"Application"`
-	Version      string `json:"Version"`
-	SuccessCount int    `json:"Success_Count"`
+	Application  string
+	Version      string
+	SuccessCount int `json:"Success_Count"`
 }
